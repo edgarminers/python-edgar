369e78e79af09524ca4885304c0382cd94eeba30
Edouard Swiac <edouard.swiac@gmail.com>
2018-06-23 14:07:22 -0700

    release 2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='python-edgar',
-    version='2.2',
+    version='2.0',
     description='Download the SEC fillings index from EDGAR since 1993',
     long_description=read('README.md'),
     author='Edouard Swiac',
