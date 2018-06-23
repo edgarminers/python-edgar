32dc7966b739e53a0ab5d95cf6f01fbbacd07b72
Edouard Swiac <eswiac@twitter.com>
2018-06-22 17:22:48 -0700

    cleanup + use https instead of ftp. support python 2 and 3. add parallel download

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
 from distutils.core import setup
 
 setup(name='python-edgar',
-      version='1.0',
-      description='A downloader of the SEC EDGAR index since 1993 (company name, form type, EDGAR form url)',
+      version='2.0',
+      description='Download the SEC EDGAR index since 1993 (company name, form type, EDGAR form url)',
       author='Edouard Swiac',
       author_email='edouard.swiac@gmail.com',
       url='https://github.com/edouardswiac/python-edgar',
       packages=['edgar'],
       license="MIT License",
       classifiers = [
-          "Development Status :: 4 - Beta",
+          "Development Status :: 5 - Production/Stable",
           "License :: OSI Approved :: MIT License",
           "Intended Audience :: Developers",
           "Intended Audience :: Information Technology",
           "Intended Audience :: Financial and Insurance Industry",
           "Operating System :: OS Independent",
-          "Programming Language :: Python",
           "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: Implementation :: CPython",
-          "Programming Language :: Python :: Implementation :: PyPy",
+          "Programming Language :: Python :: 3"
           "Topic :: Office/Business :: Financial",
           "Topic :: Office/Business :: Financial :: Investment"
       ],
