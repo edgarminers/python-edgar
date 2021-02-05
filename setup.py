89fd407589da18ba06c15e72c3eb6e3e584206da
Edouard Swiac <edouard.swiac@gmail.com>
2021-02-04 22:12:42 -0800

    minor bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="python-edgar",
-    version="3.0.1",
+    version="3.0.2",
     description="Download the SEC fillings index from EDGAR since 1993",
     long_description=long_description,
     long_description_content_type="text/markdown",
